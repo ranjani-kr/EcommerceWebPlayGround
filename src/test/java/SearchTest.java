9a1f623f9da13e392ca1723361e4433f80167fcd
Ranjani KR <ranjanikuduvalliraveendra@gmail.com>
2023-12-04 23:47:21 +1100

    Adding outline for search Test with AAA approach

---
+++ src/test/java/SearchTest.java
@@ -0,0 +1,40 @@
+import org.testng.annotations.Test;
+
+
+public class SearchTest {
+
+
+        @Test
+        public void verifySearchResultShowsRelevantResults(){
+
+        /* Arrangegi
+        1. Go to WebPlayground : Url : "https://web-playground.ultralesson.com/"
+        LauncherPage launcherPage = new LauncherPage(webDriver);
+        launcherPage.navigateTo(""https://web-playground.ultralesson.com/"");
+        2. Search product : keyword - Jeans
+        String searchProduct = "Jeans";
+        String searchKey = "Jean";
+        3. Search result :
+
+
+         Act
+        1. Navigate to URL
+        HomePage homePage = new HomePage(webDriver);
+
+
+        2. Click on Search Icon
+        3. Type Search keyword
+
+        homePage.search(Jeans);
+
+        4. verify search result
+        List<item> searchItems = homepage.getSearchItems;
+
+        Assert
+        1.Verify the product name
+        Assertions.assertEquals(4,searchItems.size());
+        Assertions.assertTrue(searchItems.stream().allMatch(item -> item.getName().contains(searchKey)));
+        */
+        }
+
+}
